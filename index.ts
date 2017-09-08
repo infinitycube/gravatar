5774ec067dd03204457cd2e3ab93d17f08d83931
Jake Smith <jakehockey10@gmail.com>
2017-09-08 12:10:35 -0600

    try the service imported first.

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export { GravatarDirective } from './src/gravatar.directive';
 export { GravatarService } from './src/gravatar.service';
+export { GravatarDirective } from './src/gravatar.directive';
 export { GravatarModule } from './src/gravatar.module';
